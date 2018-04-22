1fe8f6fef022225b895462b69c5d5e3829c00bfc
Lucky Kurniawan <kurniawanluckyy@gmail.com>
2018-04-22 11:58:33 +0800

    [ADD][10.0]account_invoice_filter_d_w_m: * Add Invoice Filter Today, Week, Month, Year

---
+++ account_invoice_filter_d_w_m/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017
+#   @Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Invoice Filter Today, Week, Month, Year",
+    "summary": "Add Filter Today, This Week, This Month,and This year",
+    "version": "10.0.1.0.0",
+    "author": "Lucky Kurniawan ",
+    "website": "https://github.com/kurniawanlucky/Odoo10.0",
+    "category": "Account",
+    "depends": ['account'],
+    "data": [
+        'views/account_invoice_views.xml',
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+}
