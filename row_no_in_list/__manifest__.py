e6fdcfbed9d1b2056ee8f3ea57920baee20ecc6e
Lucky Kurniawan <kurniawanluckyy@gmail.com>
2020-06-28 17:08:23 +0800

    [ADD][10.0]row_no_in_list

--- row_no_in_list/__manifest__.py
+++ row_no_in_list/__manifest__.py
@@ -3,24 +3,25 @@
 #   @Lucky Kurniawan <kurniawanluckyy@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Row Number in List',
-    'version': '10.0.0',
-    'category': 'Other',
-    'description': """
+    "name": "Row Number in List",
+    "version": "10.0.0",
+    "category": "Other",
+    "summary": "You Will See the Number Inside List View",
+    "description": """
 Add Row Number in List View.
 """,
-    'author': 'Lucky Kurniawan',
-    'website': '',
-    'images': [],
-    'depends': ['web'],
-    'data': [],
-    'qweb': [
-       'static/src/xml/base.xml',
+    "author": "Lucky Kurniawan",
+    "website": "https://github.com/kurniawanlucky/Odoo10.0",
+    "images": [],
+    "depends": ["web"],
+    "data": [],
+    "qweb": [
+       "static/src/xml/base.xml",
     ],
-    'images': ['static/description/icon.png'],
-    'demo': [],
-    'test': [],
-    'license': 'AGPL-3',
-    'installable': True,
-    'auto_install': False,
+    "images": ["static/description/icon.png"],
+    "demo": [],
+    "test": [],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
 }
