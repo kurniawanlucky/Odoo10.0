034e49bec24111cb6d4dd4966b1465da9d3d0670
Lucky Kurniawan <kurniawanluckyy@gmail.com>
2020-12-06 16:50:22 +0800

    test

--- booking_service/__manifest__.py
+++ booking_service/__manifest__.py
@@ -34,6 +34,7 @@ To allow users to create bookings for employees and equipments.
     "test": [],
     "currency": "EUR",
     "license": "AGPL-3",
+    "images": ['static/description/1.png'],
     "installable": True,
     "application": True,
 }
