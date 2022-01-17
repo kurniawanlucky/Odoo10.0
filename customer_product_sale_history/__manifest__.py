3e8d9a633436748a3e4ea3c96be16386c1823312
lucky <kurniawnaluckyy@gmail.com>
2022-01-17 21:57:44 +0800

    [ADD][15.0]customer_product_sale_history

---
+++ customer_product_sale_history/__manifest__.py
@@ -0,0 +1,27 @@
+# © 2022 Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale Customer History - Sale Order',
+    'version': '1.1',
+    'category': 'Sale Management',
+    'description': """
+Sale Order.
+===========
+Sale Customer History:
+----------------------
+    * Show all History Transaction Customer at Sales Order Tab Page
+""",
+    'author': 'Lucky Kurniawan<kurniawanluckyy@gmail.com>',
+    'website': '',
+    'images': ['static/description/icon.png'],
+    'depends': ['sale'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'demo': [],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ customer_product_sale_history/models/res_partner.py
@@ -0,0 +1,28 @@
+# © 2022 Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models, _
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    product_sale_history_ids = fields.One2many('sale.order.line', compute='_compute_customer_history_ids', string='Product Sales')
+    count_product_sale_history = fields.Integer(string="Count Product Sale", compute='_compute_customer_history_ids', readonly=False)
+
+    def action_view_sale_product(self):
+        view_id_list = self.env.ref('sale.view_order_line_tree').id
+        view_id_form = self.env.ref('sale.sale_order_line_view_form_readonly').id
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Sales'),
+            'res_model': 'sale.order.line',
+            'target': 'current',
+            'domain': [('id', 'in', self.product_sale_history_ids.ids)],
+            'views': [[view_id_list, 'list'], [view_id_form, 'form']],
+            'context': {'search_default_product': True},
+        }
+
+    def _compute_customer_history_ids(self):
+        for partner in self:
+            partner.product_sale_history_ids = partner.sale_order_ids.mapped('order_line')
+            partner.count_product_sale_history = len(partner.sale_order_ids.mapped('order_line'))
